e92e183de7db36c7665cd6fe3ac916a2a7d1e372
Augusto Melo <augustinho.melo@hotmail.com>
2017-12-04 21:30:29 -0200

    Contando vidas

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -57,6 +57,7 @@ public class GM : MonoBehaviour {
 
 	void DisplayHudData() {
 		ui.hud.txtCoinCount.text = "x " + data.coinCount;
+		ui.hud.txtLifeCount.text = "x " + data.lifeCount;
 		ui.hud.txtTimer.text = "Timer: " + timeLeft.ToString("F1");
 	}
 
@@ -67,8 +68,19 @@ public class GM : MonoBehaviour {
 	public void KillPlayer(){
 		if(player != null){
 			Destroy(player.gameObject);
-			Invoke("RespawnPlayer",timeToRespawn);
+			DecrementLives();
+			if (data.lifeCount > 0){
+			Invoke("RespawnPlayer", timeToRespawn);
 		}
+		else {
+			GameOver();
+		}
+
+	}
+}
+
+	public void DecrementLives(){
+		data.lifeCount--;
 	}
 
 	public void ExpirePlayer(){
